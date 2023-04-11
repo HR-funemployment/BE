977430b0bb624cd952447bd3b35fe4015755900c
David Zhang <david.zny1022@gmail.com>
2023-04-11 01:48:41 -0700

    set up server + eslint

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+//  ====== Imports =========
+import dotenv from 'dotenv';
+import express, { Application } from 'express';
+import cors from 'cors';
+import logger from './middleware/logger';
+import morgan from './middleware/morgan';
+
+//  ====== Constants =========
+dotenv.config();
+const app: Application = express();
+const port = process.env.Port || 8080;
+
+//  ====== Middleware =========
+app.use(express.json());
+app.use(cors());
+app.use(morgan);
+
+app.listen(port, () => {
+  logger.info(`Server is running at port ${port}`);
+});
---
+++ src/middleware/logger.ts
@@ -0,0 +1,56 @@
+import winston, { format, Logger } from 'winston';
+
+export interface LogEntry {
+  level: string;
+  message: string;
+  timestamp: string;
+}
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+const loggerLevel = () => {
+  const env = process.env.NODE_ENV || 'development';
+  const isDevelopment = env === 'development';
+  return isDevelopment ? 'debug' : 'warn';
+};
+
+const colors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'blue',
+  debug: 'white',
+};
+
+winston.addColors(colors);
+
+const logFormat = format.printf((info) => {
+  const { timestamp, level, message } = info as LogEntry;
+  return `${timestamp} ${level}: ${message}`;
+});
+
+const logger: Logger = winston.createLogger({
+  level: loggerLevel(),
+  levels,
+  format: format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+    format.colorize({ all: true }),
+    logFormat
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: 'logs/error.log',
+      level: 'error',
+    }),
+    new winston.transports.File({ filename: 'logs/all.log' }),
+  ],
+});
+
+export default logger;
