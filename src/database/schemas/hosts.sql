4c3cf1bdeced99eeb091b69305f603c48bb28711
David Zhang <david.zny1022@gmail.com>
2023-04-19 23:40:20 -0700

    schemas for listing drafts and hosts

---
+++ src/database/schemas/hosts.sql
@@ -0,0 +1,23 @@
+CREATE TABLE hosts (
+  host_id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  host_url TEXT,
+  host_name TEXT,
+  host_since DATE,
+  host_location TEXT,
+  host_about TEXT,
+  host_response_time TEXT,
+  host_response_rate TEXT,
+  host_acceptance_rate TEXT,
+  host_is_superhost BOOLEAN,
+  host_thumbnail_url TEXT,
+  host_picture_url TEXT,
+  host_neighbourhood TEXT,
+  host_listings_count INTEGER,
+  host_total_listings_count INTEGER,
+  host_verifications TEXT [],
+  host_has_profile_pic BOOLEAN,
+  host_identity_verified BOOLEAN,
+);
+
+CREATE INDEX idx_user_id ON hosts(user_id);
---
+++ src/database/schemas/listing_drafts.sql
@@ -0,0 +1,51 @@
+-- User is able to create multiple drafts of listings
+CREATE TABLE listing_drafts (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  host_id INTEGER REFERENCES hosts(id),
+  structure_type TEXT NOT NULL,
+  room_type TEXT,
+  -- entire/private/shared in structure -> connected with structure_type
+  property_type TEXT,
+  -- Address and map
+  street_address VARCHAR(255) NOT NULL,
+  apt VARCHAR(50),
+  city VARCHAR(100) NOT NULL,
+  state_province_territory VARCHAR(100) NOT NULL,
+  postal_code VARCHAR(20) NOT NULL,
+  full_address TEXT,
+  neighborhood_overview TEXT,
+  neighbourhood TEXT,
+  neighbourhood_cleansed TEXT,
+  latitude NUMERIC(9, 6),
+  longitude NUMERIC(9, 6),
+  --------------- floorplan
+  bedrooms INTEGER,
+  beds INTEGER,
+  accommodates INTEGER,
+  bathrooms NUMERIC(2, 1),
+  lock_available BOOLEAN,
+  -- 1.5 baths/shared baths/private bath
+  -- Me / My Family / Other Guests/ Roommates -Private room only
+  other_guests VARCHAR(50),
+  bathrooms_text TEXT,
+  -- bathroom types for private rooms
+  private_bathrooms NUMERIC(2, 1),
+  dedicated_bathrooms NUMERIC(2, 1),
+  shared_bathrooms NUMERIC(2, 1),
+  ------------------------ amenities
+  amenities TEXT [],
+  standout_amenities TEXT [],
+  safety_items TEXT [],
+  picture_url TEXT,
+  property_photos TEXT [],
+  name TEXT NOT NULL,
+  highlights TEXT [],
+  description TEXT NOT NULL,
+  allows_any_guests BOOLEAN,
+  price NUMERIC(7, 2),
+  offering_discounts BOOLEAN,
+  legal_inclusions jsonb [],
+);
+
+CREATE INDEX idx_user_id ON listing_drafts(user_id);
